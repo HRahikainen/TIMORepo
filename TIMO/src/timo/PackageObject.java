36d495de36baecd4b9c23eb24a9b3562df5d9b27
harza <harza@harzaT470>
2017-11-21 14:35:50 +0200

    Commented for reference

--- TIMO/src/timo/PackageObject.java
+++ TIMO/src/timo/PackageObject.java
@@ -6,4 +6,6 @@ public abstract class PackageObject {
 	private boolean isFragile;
 	
 	// X amount of extending non-generic objects
+	// Objects for example conscript who breaks every time the destination is either Lappi or Eastern Finland
+	
 }

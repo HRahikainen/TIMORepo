4f5dcc361c5e6ea7aa629faa6ef4d948e6ab6c07
Jesse <JZNT@users.noreply.github.com>
2017-12-08 15:23:52 +0200

    Added properties and methods

--- TIMO/src/timo/SamsungGalaxyNoteSeven.java
+++ TIMO/src/timo/SamsungGalaxyNoteSeven.java
@@ -2,4 +2,13 @@ package timo;
 
 public class SamsungGalaxyNoteSeven extends Item {
 
+	private boolean isBurning = true;
+
+	public boolean isBurning() {
+		return isBurning;
+	}
+
+	public void setBurning(boolean isBurning) {
+		this.isBurning = isBurning;
+	}
 }

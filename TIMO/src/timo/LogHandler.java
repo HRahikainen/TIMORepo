1682da8dde9caf9fcd28977036f621f7c50a0fad
Jesse <JZNT@users.noreply.github.com>
2017-12-16 02:19:01 +0200

    Added package serialization

--- TIMO/src/timo/LogHandler.java
+++ TIMO/src/timo/LogHandler.java
@@ -2,9 +2,13 @@ package timo;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class LogHandler {
@@ -40,5 +44,34 @@ public class LogHandler {
 		} catch (IOException ex) {
     			System.out.println("Error while writing log...");
 		}
+	}
+	
+	public static void writePackages(ArrayList<Package> packageList) {
+		try{
+	         FileOutputStream fos= new FileOutputStream("PackageListFile");
+	         ObjectOutputStream oos= new ObjectOutputStream(fos);
+	         oos.writeObject(packageList);
+	         oos.close();
+	         fos.close();
+	       }catch(IOException ioe){
+	            ioe.printStackTrace();
+	        }
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void readPackages(){
+		 try
+	        {
+	            FileInputStream fis = new FileInputStream("PackageListFile");
+	            ObjectInputStream ois = new ObjectInputStream(fis);
+	            Storage.getInstance().getPackages().addAll((ArrayList<Package>) ois.readObject());
+	            ois.close();
+	            fis.close();
+	         }catch(IOException ioe){
+	             ioe.printStackTrace();
+	          }catch(ClassNotFoundException c){
+	             System.out.println("Class not found");
+	             c.printStackTrace();
+	          }
 	}
 }
